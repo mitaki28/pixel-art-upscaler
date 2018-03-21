6c2a38e3343aa2f3f45b9e4f57cb07bccfd24156
mitaki28 <mitaki28@users.noreply.github.com>
2018-03-22 06:48:01 +0900

    fix visualize

--- facade_visualizer.py
+++ facade_visualizer.py
@@ -37,7 +37,8 @@ def out_image_base(updater, xp, n, seed, dst, converter):
             ret.append(x_in)
             ret.append(t_out)
             ret.append(x_out)
-            ret.append(resize(x_out, (x_out.shape[1] // 2, x_out.shape[2] // 2), Image.BOX))
+            _, h_out, w_out = x_out.shape
+            ret.append(resize(resize(x_out, (h_out // 2, w_out // 2), Image.BOX)), (h_out, w_out), Image.NEAREST) 
         
         def save_image(x, name, mode=None):
             _, C, H, W = x.shape
