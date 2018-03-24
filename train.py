6933f54886cca288ba2b966dd9a69ead00428b69
mitaki28 <mitaki28@users.noreply.github.com>
2018-03-25 07:29:22 +0900

    fix default parameter

--- train.py
+++ train.py
@@ -26,13 +26,13 @@ from visualizer import out_image
 
 def main():
     parser = argparse.ArgumentParser(description='chainer implementation of pix2pix')
-    parser.add_argument('--batchsize', '-b', type=int, default=4,
+    parser.add_argument('--batchsize', '-b', type=int, default=1,
                         help='Number of images in each mini-batch')
     parser.add_argument('--epoch', '-e', type=int, default=200,
                         help='Number of sweeps over the dataset to train')
     parser.add_argument('--gpu', '-g', type=int, default=-1,
                         help='GPU ID (negative value indicates CPU)')
-    parser.add_argument('--dataset', '-i', default='./image/fsm-all',
+    parser.add_argument('--dataset', '-i', default='./image/fsm',
                         help='Directory of image files.')
     parser.add_argument('--out', '-o', default='result',
                         help='Directory to output the result')
