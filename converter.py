05f275eafa2804302fd8e8642581d2c21ca9e20e
mitaki28 <mitaki28@users.noreply.github.com>
2018-03-26 06:36:08 +0900

    add image size option to converter

--- converter.py
+++ converter.py
@@ -10,7 +10,7 @@ from webdnn.frontend.chainer import ChainerConverter
 from webdnn.backend import generate_descriptor
 
 
-def export(model_dir, iteration, in_ch=4, out_ch=4, output_dir=Path(__file__).parent/'frontend'/'public'/'model'/'webdnn'):
+def export(model_dir, iteration, w_in=64, in_ch=4, out_ch=4, output_dir=Path(__file__).parent/'frontend'/'public'/'model'/'webdnn'):
     model_dir = Path(model_dir)
     output_dir = Path(output_dir)
     enc = net.Encoder(in_ch)
@@ -31,7 +31,7 @@ def export(model_dir, iteration, in_ch=4, out_ch=4, output_dir=Path(__file__).pa
             cbr.batchnorm.avg_var = np.zeros(cbr.batchnorm.avg_var.shape, cbr.batchnorm.avg_var.dtype)
 
 
-    x_in = chainer.Variable(np.zeros((1, in_ch, 64, 64), dtype=np.float32))
+    x_in = chainer.Variable(np.zeros((1, in_ch, w_in, w_in), dtype=np.float32))
     x_out = dec(enc(x_in))
     graph = ChainerConverter().convert([x_in], [x_out])
     exec_info = generate_descriptor("webgl", graph)
