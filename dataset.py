37626cc9ead08faf9eb021178863c23b6aefce2f
mitaki28 <mitaki28@users.noreply.github.com>
2018-04-07 09:22:34 +0900

    label, data をリネーム

--- dataset.py
+++ dataset.py
@@ -13,109 +13,109 @@ from chainercv.utils import read_image
 
 from util import img_to_chw_array, downscale_random_nearest_neighbor
 
-def random_crop_by_2(img, c_label, pH, pW, fH, fW):
+def random_crop_by_2(img, c_source, pH, pW, fH, fW):
     y = np.random.randint(pH)
     x = np.random.randint(pW)
-    label, data = img[:c_label], img[c_label:]
-    label = label[:,y:y+fH,x:x+fW]
-    y_data = (y // 2) * 2
-    x_data = (x // 2) * 2
-    data = data[:,y_data:y_data+fH,x_data:x_data+fW]
-    return np.concatenate([label, data], axis=0)
+    source, target = img[:c_source], img[c_source:]
+    source = source[:,y:y+fH,x:x+fW]
+    y_target = (y // 2) * 2
+    x_target = (x // 2) * 2
+    target = target[:,y_target:y_target+fH,x_target:x_target+fW]
+    return np.concatenate([source, target], axis=0)
 
 # TODO padding, resize は全部 Dataset 側でやるようにしたい
 class PairDownscaleDataset(dataset_mixin.DatasetMixin):
 
-    def __init__(self, data_dir, label_dir, char_size=(48, 48), fine_size=(64, 64)):
+    def __init__(self, target_dir, source_dir, char_size=(48, 48), fine_size=(64, 64)):
         self.char_size = char_size
         self.fine_size = fine_size
-        self.data_dir = Path(data_dir)
-        self.label_dir = Path(label_dir)
-        data_names = set([path.name for path in self.data_dir.glob("*.png")])
-        label_names = set([path.name for path in self.label_dir.glob("*.png")])
-        self.filenames = list(data_names & label_names)
+        self.target_dir = Path(target_dir)
+        self.source_dir = Path(source_dir)
+        target_names = set([path.name for path in self.target_dir.glob("*.png")])
+        source_names = set([path.name for path in self.source_dir.glob("*.png")])
+        self.filenames = list(target_names & source_names)
         print(len(self.filenames), 'loaded')
-        print(len(label_names - data_names), 'ignored from label')
-        print(len(data_names - label_names), 'ignored from data')
+        print(len(source_names - target_names), 'ignored from source')
+        print(len(target_names - source_names), 'ignored from target')
     
     def __len__(self):
         return len(self.filenames)
 
-    def argument_image(self, img, c_label, is_crop_random=True, is_flip_random=True):
+    def argument_image(self, img, c_source, is_crop_random=True, is_flip_random=True):
         cW, cH = self.char_size
         fW, fH = self.fine_size
         pW, pH = ((fW - cW), (fH - cH))
         if is_crop_random:
             assert pW >= 0 and pW % 2 == 0 and pH >= 0 and pH % 2 == 0
             img = resize_contain(img, (fH + pH, fW + pW), img[:,0,0])
-            img = random_crop_by_2(img, c_label, pH, pW, fH, fW)
+            img = random_crop_by_2(img, c_source, pH, pW, fH, fW)
         else:
             img = resize_contain(img, (fH, fW), img[:,0,0])
         if is_flip_random:
             img = random_flip(img, x_random=True)
         return img
 
-    # return (label, img)
+    # return (source, img)
     def get_example(self, i):
         filename = self.filenames[i]
-        with Image.open(self.data_dir/filename) as f:
-            img = img_to_chw_array(f)
-        with Image.open(self.label_dir/filename) as f:
-            label = img_to_chw_array(f)
-        C, H, W = label.shape
+        with Image.open(self.source_dir/filename) as f:
+            source = img_to_chw_array(f)
+        with Image.open(self.target_dir/filename) as f:
+            target = img_to_chw_array(f)
+        C, H, W = source.shape
         py, px = random.choice([(0, 0), (1, 0), (0, 1)])
-        label[:,1::2,1::2] = label[:,py::2,px::2]
+        source[:,1::2,1::2] = source[:,py::2,px::2]
         
-        c_label = label.shape[0]
-        t = np.concatenate([label, img], axis=0)
-        t = self.argument_image(t, c_label, self.char_size, self.fine_size)
+        c_source = source.shape[0]
+        t = np.concatenate([source, target], axis=0)
+        t = self.argument_image(t, c_source, self.char_size, self.fine_size)
 
         t = resize(t, (64, 64), Image.NEAREST)                
-        return t[:c_label], t[c_label:]
+        return t[:c_source], t[c_source:]
     
 class AutoUpscaleDataset(dataset_mixin.DatasetMixin):
-    def __init__(self, label_dir, random_nn=True):
-        self.label_dir = Path(label_dir)
-        self.filepaths = list(self.label_dir.glob("*.png"))
+    def __init__(self, target_dir, random_nn=True):
+        self.target_dir = Path(target_dir)
+        self.filepaths = list(self.target_dir.glob("*.png"))
         self.random_nn = random_nn
         print("{} images loaded".format(len(self.filepaths)))
     
     def __len__(self):
         return len(self.filepaths)
 
-    # return (label, img)
+    # return (source, target)
     def get_example(self, i):
         with Image.open(str(self.filepaths[i])) as f:
-            img = img_to_chw_array(f)
+            target = img_to_chw_array(f)
 
-        img = random_crop(img, (64, 64))
-        img = random_flip(img, x_random=True)
+        target = random_crop(target, (64, 64))
+        target = random_flip(target, x_random=True)
         if self.random_nn:
-            label = resize(downscale_random_nearest_neighbor(img), (64, 64), Image.NEAREST)
+            source = resize(downscale_random_nearest_neighbor(target), (64, 64), Image.NEAREST)
         else:
-            label = resize(resize(img, (32, 32), Image.NEAREST), (64, 64), Image.NEAREST)
-        img = resize(img, (64, 64), Image.NEAREST)
-        return label, img
+            source = resize(resize(target, (32, 32), Image.NEAREST), (64, 64), Image.NEAREST)
+        target = resize(target, (64, 64), Image.NEAREST)
+        return source, target
 
 class AutoUpscaleDatasetReverse(AutoUpscaleDataset):
 
-    def __init__(self, label_dir):
-        self.label_dir = Path(label_dir)
-        self.filepaths = list(self.label_dir.glob("*.png"))
+    def __init__(self, target_dir):
+        self.target_dir = Path(target_dir)
+        self.filepaths = list(self.target_dir.glob("*.png"))
         print("{} images loaded".format(len(self.filepaths)))
 
     def get_example(self, i):
         with Image.open(str(self.filepaths[i])) as f:
-            label = img_to_chw_array(f)
-        img = label.copy()
-        C, H, W = label.shape
+            source = img_to_chw_array(f)
+        img = source.copy()
+        C, H, W = source.shape
         # py, px = random.choice([(0, 0), (1, 0), (0, 1)])
-        # label[:,1::2,1::2] = label[:,py::2,px::2]
+        # source[:,1::2,1::2] = source[:,py::2,px::2]
 
-        c_label = label.shape[0]
-        t = np.concatenate([label, img], axis=0)
+        c_source = source.shape[0]
+        t = np.concatenate([source, img], axis=0)
 
         t = center_crop(t, (64, 64))
         t = random_flip(t, x_random=True)
         t = resize(t, (64, 64), Image.NEAREST)
-        return t[:c_label], t[c_label:]
+        return t[:c_source], t[c_source:]
